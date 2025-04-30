3c18525e706689c9f36698c2be288b84deddc2cf
Igor <i.gembaruk@mail.ru>
2025-04-30 14:43:55 +0300

    [Task3-Lesson1_Naming]add feature: add task3

---
+++ src/main/kotlin/lesson1/task3.kt
@@ -0,0 +1,13 @@
+package ru.gembaruk.lesson1
+
+fun main(){
+    val year = 1961
+    var hour = 9
+    var minute = 7
+    println(year)
+    println(hour)
+    println(minute)
+    hour = 10
+    minute = 55
+    print("$hour:$minute")
+}
