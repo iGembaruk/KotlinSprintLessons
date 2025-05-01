640c7b1c3f2a73444889ce41118ac504b3266612
Igor <i.gembaruk@mail.ru>
2025-05-01 09:30:18 +0300

    [task5-lesson1_naming] feat: add task5

--- src/main/kotlin/lesson1/task3.kt
+++ src/main/kotlin/lesson1/task3.kt
@@ -1,13 +1,13 @@
 package ru.gembaruk.lesson1
 
 fun main(){
-    val year = 1961
-    var hour = 9
-    var minute = 7
+    val year = "1961"
+    var hour = "09"
+    var minute = "07"
     println(year)
     println(hour)
     println(minute)
-    hour = 10
-    minute = 55
-    print("$hour:$minute")
+    hour = "10"
+    minute = "55"
+    println("$hour:$minute")
 }
