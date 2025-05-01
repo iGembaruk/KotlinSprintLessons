b4f13bf2ac197da7c92b85b8c5451c15c879c983
Igor <i.gembaruk@mail.ru>
2025-05-01 12:07:17 +0300

    [task3-ARITHMETIC_LESSON2] refactoring constants task3

--- src/main/kotlin/lesson2/task3.kt
+++ src/main/kotlin/lesson2/task3.kt
@@ -1,14 +1,15 @@
 package ru.gembaruk.lesson2
 
-const val hoursDeparture = 9
-const val minutesDeparture = 39
-const val minutesInPath = 457
-fun main() {
-    val fullsHoursInPath = minutesInPath / 60
-    val minutesAfterFullHoursInPatch = minutesInPath % 60
+const val HOURS_DEPARTURE = 9
+const val MINUTES_DEPARTURE = 39
+const val MINUTES_IN_PATCH = 457
 
-    var hoursArrival = hoursDeparture + fullsHoursInPath
-    var minutesArrival = minutesAfterFullHoursInPatch + minutesDeparture
+fun main() {
+    val fullsHoursInPath = MINUTES_IN_PATCH / 60
+    val minutesAfterFullHoursInPatch = MINUTES_IN_PATCH % 60
+
+    var hoursArrival = HOURS_DEPARTURE + fullsHoursInPath
+    var minutesArrival = minutesAfterFullHoursInPatch + MINUTES_DEPARTURE
     if(minutesArrival >= 60){
         hoursArrival += 1
         minutesArrival -= 60
