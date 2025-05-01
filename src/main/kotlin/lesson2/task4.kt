a90c53aa5e37d315314325f0f9eec7bb059f86d5
Igor <i.gembaruk@mail.ru>
2025-05-01 17:52:02 +0300

    [ks-2-4] refactoring task4

--- src/main/kotlin/lesson2/task4.kt
+++ src/main/kotlin/lesson2/task4.kt
@@ -1,12 +1,13 @@
 package ru.gembaruk.lesson2
 
-const val COUNT_CRYSTALS = 7
-const val COUNT_IRONS = 11
 const val BONUS = 0.2
 
 fun main() {
-    val bonusCrystals = COUNT_CRYSTALS * BONUS
-    val bonusIrons = COUNT_IRONS * BONUS
+    var countCrystals = 7
+    var bonusCrystals = countCrystals * BONUS
+
+    var countIrons = 11
+    var bonusIrons = countIrons * BONUS
     println(bonusCrystals.toInt())
     println(bonusIrons.toInt())
 }
