a50af8f7583d230560e09a8d945a1182cbb66dce
Igor <i.gembaruk@mail.ru>
2025-05-01 17:19:29 +0300

    [task1-ARITHMETIC_LESSON2] refactoring task1

--- src/main/kotlin/lesson2/task1.kt
+++ src/main/kotlin/lesson2/task1.kt
@@ -1,11 +1,14 @@
 package ru.gembaruk.lesson2
 
-const val GRADE_STUDENT1 = 3
-const val GRADE_STUDENT2 = 4
-const val GRADE_STUDENT3 = 3
-const val GRADE_STUDENT4 = 5
 
 fun main(){
-    val avg = (GRADE_STUDENT1 + GRADE_STUDENT2 + GRADE_STUDENT3 + GRADE_STUDENT4) / 4.0;
+     val COUNT_STUDENTS = 4
+
+     var GRADE_STUDENT1 = 3.0
+     var GRADE_STUDENT2 = 4.0
+     var GRADE_STUDENT3 = 3.0
+     var GRADE_STUDENT4 = 5.0
+
+     var avg = (GRADE_STUDENT1 + GRADE_STUDENT2 + GRADE_STUDENT3 + GRADE_STUDENT4) / COUNT_STUDENTS
     println(avg)
 }
