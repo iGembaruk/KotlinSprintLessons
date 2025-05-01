d74358fc6febee9ac3e01c120678023ae21e2a60
Igor <i.gembaruk@mail.ru>
2025-05-01 12:04:50 +0300

    [task2-ARITHMETIC_LESSON2] add task2

--- src/main/kotlin/lesson2/task2.kt
+++ src/main/kotlin/lesson2/task2.kt
@@ -1,14 +1,14 @@
 package ru.gembaruk.lesson2
 
-const val countPermanentPersonals = 50
-const val salaryPermanentPersonals = 30_000
-const val countInterns = 30
-const val salaryInterns = 20_000
+const val COUNT_PERMANENT_PERSONAL = 50
+const val SALARY_PERMANENT_PERSONAL = 30_000
+const val COUNT_INTERNS = 30
+const val SALARY_INTERN = 20_000
 
 fun main(){
-    val totalSalaryPermanentPersonals = countPermanentPersonals * salaryPermanentPersonals
-    val sumFullPeoples = totalSalaryPermanentPersonals + (salaryInterns * countInterns)
-    val avgSalary = sumFullPeoples / (countInterns + countPermanentPersonals)
+    val totalSalaryPermanentPersonals = COUNT_PERMANENT_PERSONAL * SALARY_PERMANENT_PERSONAL
+    val sumFullPeoples = totalSalaryPermanentPersonals + (SALARY_INTERN * COUNT_INTERNS)
+    val avgSalary = sumFullPeoples / (COUNT_INTERNS + COUNT_PERMANENT_PERSONAL)
     println("Расходы на выплату зарплаты постоянных сотрудников: $totalSalaryPermanentPersonals")
     println("Общие расходы по ЗП после прихода стажеров: $sumFullPeoples")
     println("Средняя ЗП одного сотрудника после устройства стажеров: $avgSalary")
