3564e8e1d969ed2a6074437518794fcbb67eeeaa
Igor <i.gembaruk@mail.ru>
2025-05-01 10:31:10 +0300

    [task1-ARITHMETIC] add task1

---
+++ src/main/kotlin/lesson2/task1.kt
@@ -0,0 +1,10 @@
+package ru.gembaruk.lesson2
+
+fun main(){
+    val gradeStudent1 = 3
+    val gradeStudent2 = 4
+    val gradeStudent3 = 3
+    val gradeStudent4 = 5
+    val avg1 = (gradeStudent1 + gradeStudent2 + gradeStudent3 + gradeStudent4) / 4.0;
+    println(avg1)
+}
