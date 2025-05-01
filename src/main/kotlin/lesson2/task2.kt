4ef1769f7c70d6fd2040cf2bfa7116015a389bb2
Igor <i.gembaruk@mail.ru>
2025-05-01 17:29:39 +0300

    [ks-2-2] refactoring task2

--- src/main/kotlin/lesson2/task2.kt
+++ src/main/kotlin/lesson2/task2.kt
@@ -1,14 +1,14 @@
 package ru.gembaruk.lesson2
 
-const val COUNT_PERMANENT_PERSONAL = 50
-const val SALARY_PERMANENT_PERSONAL = 30_000
-const val COUNT_INTERNS = 30
-const val SALARY_INTERN = 20_000
-
 fun main(){
-    val totalSalaryPermanentPersonals = COUNT_PERMANENT_PERSONAL * SALARY_PERMANENT_PERSONAL
-    val sumFullPeoples = totalSalaryPermanentPersonals + (SALARY_INTERN * COUNT_INTERNS)
-    val avgSalary = sumFullPeoples / (COUNT_INTERNS + COUNT_PERMANENT_PERSONAL)
+    var countPermanentPersonal = 50
+    var salaryPermanentPersonal = 30_000
+    var countInterns = 30
+    val salaryIntern = 20_000
+
+    var totalSalaryPermanentPersonals = countPermanentPersonal * salaryPermanentPersonal
+    var sumFullPeoples = totalSalaryPermanentPersonals + (salaryIntern * countInterns)
+    var avgSalary = sumFullPeoples / (countInterns + countPermanentPersonal)
     println("Расходы на выплату зарплаты постоянных сотрудников: $totalSalaryPermanentPersonals")
     println("Общие расходы по ЗП после прихода стажеров: $sumFullPeoples")
     println("Средняя ЗП одного сотрудника после устройства стажеров: $avgSalary")
