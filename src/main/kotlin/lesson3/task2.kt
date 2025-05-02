52d5dbe94656b7d4d61e6967b8a08d24d47ed230
Igor <i.gembaruk@mail.ru>
2025-05-02 10:33:50 +0300

    [ks-3-2] add task2

---
+++ src/main/kotlin/lesson3/task2.kt
@@ -0,0 +1,11 @@
+package ru.gembaruk.lesson3
+
+fun main(){
+    var age = 20
+    var lastName = "Андреева Татьяна Сергеевна"
+    println("$lastName, $age")
+
+    age = 22
+    lastName = "Сидорова Татьяна Сергеевна"
+    println("$lastName, $age")
+}
