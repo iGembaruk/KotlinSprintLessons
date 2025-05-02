24aca541057b54a6e5c2039ef80c807a4110bf07
Igor <i.gembaruk@mail.ru>
2025-05-02 11:06:23 +0300

    [ks-3-4] add task4

---
+++ src/main/kotlin/lesson3/task4.kt
@@ -0,0 +1,12 @@
+package ru.gembaruk.lesson3
+
+fun main() {
+    var pointA = "E2"
+    var pointB = "E4"
+    println("$pointA -> $pointB")
+
+    pointA = "D2"
+    pointB = "D3"
+    println("$pointA -> $pointB")
+    println(pointB)
+}
