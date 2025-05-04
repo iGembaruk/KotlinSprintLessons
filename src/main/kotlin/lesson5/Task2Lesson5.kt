6cc7d79bd4af79cc53eb36d36f3749e7b30b13b3
Igor <i.gembaruk@mail.ru>
2025-05-05 01:15:11 +0300

    ks-5-2 refactoring task2

--- src/main/kotlin/lesson5/Task2Lesson5.kt
+++ src/main/kotlin/lesson5/Task2Lesson5.kt
@@ -5,9 +5,8 @@ const val CURRENT_YEAR = 2025
 
 fun main() {
     println("Введите свой год рождения: ")
-    val age = readLine()?.toInt()
-
-    val calculatorAge = CURRENT_YEAR - age!!
+    val age = readln().toIntOrNull() ?: CURRENT_YEAR
+    val calculatorAge = CURRENT_YEAR - age
 
     if(calculatorAge >= AGE){
         println("Показать экран со скрытым контентом")
