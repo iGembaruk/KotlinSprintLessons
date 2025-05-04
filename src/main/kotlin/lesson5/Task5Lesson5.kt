3cb3c4188ac2cb947aef16848749f31282a4d13b
Igor <i.gembaruk@mail.ru>
2025-05-05 01:38:22 +0300

    ks-5-5, refactoring task5

--- src/main/kotlin/lesson5/Task5Lesson5.kt
+++ src/main/kotlin/lesson5/Task5Lesson5.kt
@@ -1,16 +1,22 @@
 package ru.gembaruk.lesson5
 
-import kotlin.random.Random
-
 fun main() {
-    val bingoNumbers = listOf(Random.nextInt(0, 43),Random.nextInt(0, 43), Random.nextInt(0, 43))
+    val bingoNumbers = listOf(
+        (1..43).random(),
+        (1..43).random(),
+        (1..43).random()
+    )
 
     println("Введите 3 числа от \"0\" до \"42\"")
-    val userNumbers = listOf(readLine()?.toInt(), readLine()?.toInt(), readLine()?.toInt())
+    val userNumbers = listOf(
+        readln()?.toInt(),
+        readln()?.toInt(),
+        readln()?.toInt()
+    )
     val sizeUserNumbers = userNumbers.size
 
     val countCross = userNumbers.intersect(bingoNumbers).size
-    when(countCross){
+    when (countCross) {
         3 -> println("пользователь угадал все числа и выиграл джекпот")
         2 -> println("пользователь угадал два числа и получает крупный приз")
         1 -> println("пользователю выплачивается утешительный приз")
