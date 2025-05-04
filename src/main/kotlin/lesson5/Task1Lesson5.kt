a476700d1fbd09e1ddce05acc886e1dc47d3b726
Igor <i.gembaruk@mail.ru>
2025-05-05 00:52:48 +0300

    ks-5-1 refactoring task1

--- src/main/kotlin/lesson5/Task1Lesson5.kt
+++ src/main/kotlin/lesson5/Task1Lesson5.kt
@@ -1,13 +1,12 @@
 package ru.gembaruk.lesson5
 
-import kotlin.random.Random
 
 fun main() {
-    val numberOne = Random.nextInt(1, 10);
-    val numberTwo = Random.nextInt(1, 10);
+    val numberOne = (1..10).random()
+    val numberTwo = (1..10).random()
 
     println("Сложи число: $numberOne с числом: $numberTwo. Напиши ответ:")
-    val result = readLine()?.toInt()
+    val result = readln()?.toInt()
     if(result == numberOne + numberTwo) {
         println("Добро пожаловать!")
     } else {
